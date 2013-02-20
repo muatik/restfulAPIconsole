75bf8310edcf281f3586cec41f739bef8d77291c
muatik <muatik@gmail.com>
2013-02-21 01:19:05 +0200

    new api form added

--- index.php
+++ index.php
@@ -20,63 +20,6 @@
 <body>
 
 
-<div id="sample"></div>
-
-<script>
-/*
-
-example of apicall.json
-
-var APICalls=[
-	{
-		title: "Start an analysis",
-		description: "bla bla bla"
-		examples:[
-			{
-				title: "simple hourly analysis",	
-				headers:[
-					{name: "host", value: "trend.botego.net"}
-				],
-				path: "/analysis/hourly/2015-5555",
-				requestMethod:"POST",
-				postField:[
-				]
-			},
-			{
-				title: "simple daily analysis with restrictions",	
-				headers:[
-					{name: "host", value: "trend.botego.net"}
-				],
-				path: "/analysis/hourly/2015-5555",
-				requestMethod:"POST",
-				postField:[
-					{name: "language", value:"tr"},
-					{name: "gender", value:"male"}
-				]
-			}
-		]
-	}
-]
- */
-
-
-var t={
-	container:"#sample",
-	headers:[
-		{name:"HOST",value:"training.kelimeci.net"}
-	],
-	path:"/",
-	requestMethod:"GET",
-	postFields:[
-		{name:"email",value:"muatik@gmail.com"}
-	] 
-};
-
-//$.uAPIconsole(t);
-
-
-</script>
-
 </body>
 </html>
 
