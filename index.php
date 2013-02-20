8e95f32868f4539061601e08aaa5efee0749cb70
muatik <muatik@gmail.com>
2013-02-20 20:29:28 +0200

    javascript api codes rewritten as jQuery plugin

--- index.php
+++ index.php
@@ -6,42 +6,72 @@
 
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/api.js"></script>
+<script type="text/javascript" src="js/gcp/prettify.js"></script>
 
+<link href="css/prettify.css" type="text/css" rel="stylesheet" />
 <link rel="stylesheet" type="text/css" href="css/reset.css" />
 <link rel="stylesheet" type="text/css" href="css/form.css" />
 
 </head>
 <body>
 
-<form class="APIForm" action="?" method="post">
-	<div class="fl"><label>EXAMPLES: </label><select class="examples">
-		<option value="GET">Starts an analysis</option>
-	</select></div>
-	<div class="fl"><label>TYPE: </label><select class="type">
-		<option value="GET">GET</option>
-		<option value="POST">POST</option>
-		<option value="PUT">PUT</option>
-		<option value="DELETE">DELETE</option>
-	</select></div>
-	<div class="fl"><label>HOST: </label><input type="text" class="host"
-		value="localhost" /> </div>
-	<div class="fl"><label>PATH: </label><input type="text" class="path" 
-		value="trend/trendAnalysis/API/console/test.php" /> </div>
-	<div class="fl"><label>POST FIELDS: </label>
-		<a href="#" class="addField">Add field</a>
-			<ul class="postFields">
-			<li>
-				<label>Name: </label><input class="n" type="text" />
-				<label>Value: </label><input class="v" type="text" />
-				<a href="" class="remove">Remove</a>
-			</li>
-		</ul>
-	</div>
-	<div class="fl">
-		<input type="submit" value="Submit" />
-		<input type="reset" value="Reset" />
-	</div>
-</form>
+
+<div id="sample"></div>
+
+<script>
+/*
+
+example of apicall.json
+
+var APICalls=[
+	{
+		title: "Start an analysis",
+		description: "bla bla bla"
+		examples:[
+			{
+				title: "simple hourly analysis",	
+				headers:[
+					{name: "host", value: "trend.botego.net"}
+				],
+				path: "/analysis/hourly/2015-5555",
+				requestMethod:"POST",
+				postField:[
+				]
+			},
+			{
+				title: "simple daily analysis with restrictions",	
+				headers:[
+					{name: "host", value: "trend.botego.net"}
+				],
+				path: "/analysis/hourly/2015-5555",
+				requestMethod:"POST",
+				postField:[
+					{name: "language", value:"tr"},
+					{name: "gender", value:"male"}
+				]
+			}
+		]
+	}
+]
+ */
+
+
+var t={
+	container:"#e",
+	headers:[
+		{name:"HOST",value:"training.kelimeci.net"}
+	],
+	path:"/",
+	requestMethod:"GET",
+	postFields:[
+		{name:"email",value:"muatik@gmail.com"}
+	] 
+};
+
+$.uAPIconsole(t);
+
+
+</script>
 
 </body>
 </html>
