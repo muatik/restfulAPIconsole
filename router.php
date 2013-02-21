c2e16864ae08851c76d0f22c43d04ba78078c2b3
alpayOnal <alpaycom@gmail.com>
2013-02-21 02:25:18 +0200

    router script updated

--- router.php
+++ router.php
@@ -1,24 +1,37 @@
 <?php
 require_once('quickCurl.php');
-
-// post edilecek veriler
-//$data=array('language'=>'tr','name'=>'alpay');
-//$postdata = http_build_query($data);
 $r=$_REQUEST;
-
-//$path='analysis/cached';
-//$path='analysis/'.$analysisId."/events/".$event;
-//$path='analysis/'.$analysisId;
-//$path='analysis/'.$interval.'/'.$date;
 
 $path=$r['path'];
 $version=$r['version'];
 $method=$r['method'];
 
-foreach($r['headerFieldNs'] as $i=>$n)
-	if($n=='HOST')
-		$host=$r['headerFieldVs'][$i];
+if (isset($r['headerFieldNs'])){
+	
+	$headers=array();
+	
+	foreach($r['headerFieldNs'] as $i=>$n){
+		if($n=='host')
+			$host=$r['headerFieldVs'][$i];
+					
+		$headers[]=$n.':'.$r['headerFieldVs'][$i];
+		
+		$dataFields[$r['headerFieldNs'][$i]]=$r['headerFieldVs'][$i];
+	}	
+}
 
-echo quickCurl::run($host, $path, $method, $version, $headers=false);
+
+if (isset($r['dataFieldNs'])){
+	
+	$dataFields=array();
+	
+	foreach($r['dataFieldNs'] as $i=>$n)
+		$dataFields[$r['dataFieldNs'][$i]]=$r['dataFieldVs'][$i];
+	
+	$postdata = http_build_query($dataFields);
+	
+}else $postdata=null;
+
+echo quickCurl::run($host,$path,$method,$version,$headers,$postdata);
 
 ?>
